a22c16e84a497a6d11a0c5550c5d3af4c6a2d626
gaoshanyu <gaoshanyu#1122>
2018-05-13 08:33:10 +0800

    update

--- hello.rb
+++ hello.rb
@@ -1,5 +1,21 @@
-
-
-class Hello
-  puts 'Hello World!'
+def case_when
+  num = 76
+  case num
+  when ->(n) { (n % 2).zero? }
+    puts "#{num} is even"
+  else
+    puts "#{num} is odd"
+  end
 end
+
+case_when
+
+
+# module_function.rb
+# module Star
+#   def self.line
+#     puts '*' * 20
+#   end
+# end
+
+Star.line
---
+++ module.rb
@@ -0,0 +1,6 @@
+# module_function.rb
+module Star
+  def line
+    puts '*' * 20
+  end
+end
